cab1d209aa8f0b7d5d4306c1ed443801f2437c4a
xkball <45330674+xkball@users.noreply.github.com>
2026-06-18 00:59:36 +0800

    clean up
    Took 50 minutes

--- xklib-3d/src/main/java/com/xkball/xklib/XKLib.java
+++ xklib-3d/src/main/java/com/xkball/xklib/XKLib.java
@@ -3,6 +3,7 @@ package com.xkball.xklib;
 import com.xkball.xklib.utils.RegisterEventHandler;
 import com.xkball.xklib.x3d.api.event.IEvent;
 import com.xkball.xklib.x3d.api.render.IRenderContext;
+import com.xkball.xklib.x3d.backend.window.DrawTestHighLoad;
 import net.lenni0451.lambdaevents.LambdaManager;
 import net.lenni0451.lambdaevents.generator.LambdaMetaFactoryGenerator;
 
@@ -21,18 +22,8 @@ public class XKLib {
         RegisterEventHandler.runRegisterEvent();
     }
     
-//    public static void main(String[] args) {
-//        new DrawTestHighLoad().run();
-//    }
-    
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            if(b > a) System.out.println("YES");
-            else System.out.println(a % (b+1) == 0 ? "NO" : "YES");
-        }
+        new DrawTestHighLoad().run();
     }
+    
 }
